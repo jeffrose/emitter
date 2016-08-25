980b5b740dac1787c2c4c1653e410cf0a4470f3d
Jeff Rose <jefuri.rozu@gmail.com>
2016-08-25 20:42:52 +0000

    Updated the type definition

--- src/emitter.d.ts
+++ src/emitter.d.ts
@@ -1,16 +1,36 @@
 interface Emitter {
-    new(): emitter;
+    clear( type?: string ): this;
+    emit( type: string, ...args: any[] ): boolean;
+    first( type: string, ...args: any[] ): boolean;
+    off( type: string, listener: Function ): this;
+    on( type: string, listener: Function ): this;
+    once( type: string, listener: Function ): this;
+    listeners( type: string ): Function[];
+    listenerCount( type: string ): number;
+    tick( type: string, ...args: any[] ): boolean;
+    trigger( type: string, ...args: any[] ): boolean;
+    until( type: string, listener: Function ): this;
 }
 
-interface emitter {
-    clear(type?: string): this;
-    emit(type: string, ...args: any[]): boolean;
-    off(type: string, listener: Function): this;
-    on(type: string, listener: Function): this;
-    once(type: string, listener: Function): this;
-    listeners(type: string): Function[];
-    listenerCount(type: string): number;
-    tick(type: string, ...args: any[]): boolean;
-    trigger(type: string, ...args: any[]): boolean;
-    until(type: string, listener: Function): this;
+class Emitter implements Emitter {
+    static every: string;
+    static defaultMaxListeners: number;
+    static version: string;
+    
+    clear( type?: string ): this;
+    destroy(): void;
+    emit( type: string, ...args: any[] ): boolean;
+    eventTypes(): string[];
+    first( type: string, ...args: any[] ): boolean;
+    off( type: string, listener: Function ): this;
+    on( type: string, listener: Function ): this;
+    once( type: string, listener: Function ): this;
+    listeners( type: string ): Function[];
+    listenerCount( type: string ): number;
+    tick( type: string, ...args: any[] ): boolean;
+    toJSON(): object;
+    toString(): string;
+    trigger( type: string, ...args: any[] ): boolean;
+    until( type: string, listener: Function ): this;
+    valueOf(): object;
 }
